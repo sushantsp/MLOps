5443f9d41b7ddfcc6cc52403467afe28f4242f7c
sushantsp <spenshanwar007@gmail.com>
2025-07-20 21:39:43 +0530

    CI testing for changes

--- MLOps_CI/app.py
+++ MLOps_CI/app.py
@@ -16,3 +16,5 @@ fifth_power = n ** 5
 st.write(f"The square of {n} is: {square}")
 st.write(f"The cube of {n} is: {cube}")
 st.write(f"The fifth power of {n} is: {fifth_power}")
+
+st.write(f"You successfully implemented the CI pipeline.")
